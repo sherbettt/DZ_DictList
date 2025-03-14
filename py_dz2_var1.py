5b8eb7997c0e0a3750450b68ba0c2b5bcebf0ce5
Kirill K <44464893+sherbettt@users.noreply.github.com>
2025-03-14 16:13:47 +0300

    Create py_dz2_var1.py

---
+++ py_dz2_var1.py
@@ -0,0 +1,17 @@
+def fibonacci_dict(n):
+    # инициализация результата словаря
+    result = {}
+    
+    # Создание ряда Фибоначчи от нуля до макисмального
+    fib = [0, 1]
+    while fib[-1] <= n:
+        fib.append(fib[-1] + fib[-2])
+    
+    # Создание словаря для каждого индекса от 0 до n
+    for i in range(n + 1):
+        # Добавить все числа Фибоначчи, которые меньше текущего ключа.
+        result[i] = [x for x in fib if x < i]
+        
+    return result
+
+print(fibonacci_dict(5))
